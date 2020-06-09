9c842b58fba7116b8d8a1488294bfca84f4adb95
Bharat Nagda <nagdabharat@gmail.com>
2020-06-09 12:18:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {arijit:'filhaal'};
 function  updatePlaylist(object,key,value) {
   return Object.assign(object,{}[key]:'value')
 }
+
+function removeFromPlaylist(object,key) {
+  delete object.key;
+}
+
